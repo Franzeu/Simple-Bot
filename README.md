2cac75b6c3f0dda385a70a523219732c2319f9b7
Franz Eugenio <Franze2019@gmail.com>
2020-08-12 07:00:15 -0700

    Add: Clear Command and Rock-Paper-Scissors Command

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# ReddiPost-Bot
-Currently working on a notifier bot that sends a message to the discord server that the bot is in of recent reddit posts from any subreddit.
+# Simple-Bot
+Currently working on a bot that has a range of basic to advanced commands. Some commands may be fun such as rock-paper-scissors, or it could be checking the amount of players that are in the server.
 
-I have just started on making this bot using Discord.py, so only a small amount of progress have been made.
+I will be adding new commands overtime.
 
+Currently in the works :
+
+Basic Commands :
+    Clear Command - Clears certain amount of messages in discord (DONE)
+    Rock-Paper-Scissor - Plays Rock-Paper-Scissor with the user (DONE)
+    Flip Coin - Flips a coin for the user
+    Randomizer - Randomizes a list that the user wants 
+    Ping Checker : Checks ping for discord
+
+
+Advanced Commands : 
+    Coronavirus Info - Checks out the current status of the pandemic in the United States
+        Information that will be provided :
+            - Amount of confirmed cases
+            - Amount of confirmed cases that was obtained today
+            - Total Deaths
+    Reddit Subreddit Notifier - Sends a message when a new post is posted in a subreddit
--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
+import random
 from discord.ext import commands, tasks
 from itertools import cycle
 
 client = commands.Bot(command_prefix = "!")
-token = 'NzM5ODk5NDM0MzQyNjc4NTg5.XyhLCw.Kem86_GfLO5ag_Wpx_YdumDb50k'
+token = 'token' #Insert your Discord Token here
 status = cycle(['with python :)', 'around my code :D'])
 
 @client.event 
@@ -14,5 +15,59 @@ async def on_ready():
 @tasks.loop(seconds = 900)
 async def change_status():
     await client.change_presence(activity = discord.Game(next(status))) 
+
+#Clear command - deletes specified amount of messages
+@client.command()
+async def clear(ctx, amount : int):
+    await ctx.channel.purge(limit = amount)
+
+#Clear command - deletes 10000 messages
+@client.command()
+async def clearAll(ctx):
+    await ctx.channel.purge(limit = 10000)
+
+#Clear command error handling
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("**Please specify an amount of messages to delete.**")
+
+@client.command()
+async def rps(ctx, choice : str):
+    responses = ["Rock", "Paper", "Scissors"]
+    set = random.choice(responses)
+    if choice in responses:
+        await ctx.send("**Rock, Paper, Scissors, SHOOT!**")
+        if choice == set:
+            await ctx.send(f"**I picked __{set}__. This is a draw! :D**")
+        else:
+            if choice == 'Rock':
+                if set == 'Paper':
+                    await ctx.send(f"**I picked __{set}__. You lose! :(**")
+                if set == 'Scissors':
+                    await ctx.send(f"**I picked __{set}__. You Win! :)**")
+            if choice == 'Paper':
+                if set == 'Scissors':
+                    await ctx.send(f"**I picked __{set}__. You lose! :(**")
+                if set == 'Rock':
+                    await ctx.send(f"**I picked __{set}__. You Win! :)**")
+            if choice == 'Scissors':
+                if set == 'Rock':
+                    await ctx.send(f"**I picked __{set}__. You lose! :(**")
+                if set == 'Paper':
+                    await ctx.send(f"**I picked __{set}__. You Win! :)**")
+    else:
+        await ctx.send(f"**Unknown Argument. Please Capitalize the first letter.**")
+
+@rps.error
+async def rps_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("**Please input a response of either Rock, Paper, or Scissors.**")
+
+#Error handling
+@client.event
+async def error_command(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("**Invalid command used.")
 
 client.run(token)
